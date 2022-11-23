4e9b255e3a141d3ae01a7ece4e32743540f5bd5f
Eshgin <71690707+Eshgin1337@users.noreply.github.com>
2022-11-23 10:15:47 +0400

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect('mongodb+srv://esqin-admin:Esqin2002@cluster0.ak7cq.mongodb.net/usersDB');
+mongoose.connect('mongodb+srv://esqin-admin:{password}cluster0.ak7cq.mongodb.net/usersDB');
 // mongoose.connect('mongodb://localhost:27017/usersDB');
 
 const userSchema = new mongoose.Schema({
